b85e9ab0702fe8fab45209d66d9a5e0e0e3aec83
onivoid <skullyfox.yt@gmail.com>
2024-05-31 16:26:51 +0200

    CI : Formatting fix

--- backend/app/graphql/resolvers/user.py
+++ backend/app/graphql/resolvers/user.py
@@ -88,7 +88,7 @@ class Mutation:
                 return Error(message="Invalid password")
         except DoesNotExist:
             return Error(message="User not found")
-        
+
     @strawberry.field
     async def logout(self, info: strawberry.private) -> bool:
         info.context["response"].delete_cookie("token")
